75a833c57f21c9a0be95c65343540d7c4b6d46a3
Pablo de Jesús <pag753@gmail.com>
2018-11-22 15:19:22 -0600

    Initial commit.

---
+++ index.js
@@ -0,0 +1,30 @@
+//comentario
+/*
+hola
+hola2
+hola3
+*/
+var a = {
+  nombre: "Pablo",
+  apaterno: "González",
+  amaterno: "Alvarado"
+}
+console.log(a)
+var b = [0,1,2,3,4,5,6,7,8,9]
+for (var i = 0; i < b.length; i++) {
+  console.log(b[i] + "\n")
+}
+var c = 5.41
+if (c == 5) {
+console.log("c es 5")
+}
+else {
+  console.log("No lo es")
+}
+switch (c) {
+  case 5.4:
+    console.log("ok")
+    break;
+  default:
+  console.log("nop")
+}
