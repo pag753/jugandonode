4282951a4d0ce757a976b398c2c52762e630de28
Pablo de Jesús <pag753@gmail.com>
2018-11-22 16:05:11 -0600

    Consultas.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ hola
 hola2
 hola3
 */
+/*
 var a = {
   nombre: "Pablo",
   apaterno: "González",
@@ -28,3 +29,35 @@ switch (c) {
   default:
   console.log("nop")
 }
+*/
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'classicmodels'
+});
+
+connection.connect();
+/*
+connection.query('SHOW TABLES', function (error, results, fields) {
+  if (error) throw error;
+  //console.log(results);
+  for (var i = 0; i < results.length; i++)
+  {
+    console.log("Tabla número " + (i + 1) + ": " +  results[i].Tables_in_classicmodels);
+  }
+});
+*/
+
+connection.query('SELECT * FROM customers', function (error, results, fields) {
+  if (error) throw error;
+  //console.log(results);
+  //console.log(results[0]);
+  for (var i = 0; i < results.length; i++)
+  {
+    //console.log("Registro ",(i + 1),": ",results[i]);
+    console.log("Nombre: ",results[i].customerName);
+  }
+});
+connection.end();
