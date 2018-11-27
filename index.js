6a011556668352902216c90e413370426dffd0f5
Atreyu <lalo@null.net>
2018-11-27 15:26:03 -0600

    Edición.

--- index.js
+++ index.js
@@ -1,35 +1,4 @@
-//comentario
-/*
-hola
-hola2
-hola3
-*/
-/*
-var a = {
-  nombre: "Pablo",
-  apaterno: "González",
-  amaterno: "Alvarado"
-}
-console.log(a)
-var b = [0,1,2,3,4,5,6,7,8,9]
-for (var i = 0; i < b.length; i++) {
-  console.log(b[i] + "\n")
-}
-var c = 5.41
-if (c == 5) {
-console.log("c es 5")
-}
-else {
-  console.log("No lo es")
-}
-switch (c) {
-  case 5.4:
-    console.log("ok")
-    break;
-  default:
-  console.log("nop")
-}
-*/
+
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -39,16 +8,6 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
-/*
-connection.query('SHOW TABLES', function (error, results, fields) {
-  if (error) throw error;
-  //console.log(results);
-  for (var i = 0; i < results.length; i++)
-  {
-    console.log("Tabla número " + (i + 1) + ": " +  results[i].Tables_in_classicmodels);
-  }
-});
-*/
 
 connection.query('SELECT * FROM customers', function (error, results, fields) {
   if (error) throw error;
@@ -61,3 +20,7 @@ connection.query('SELECT * FROM customers', function (error, results, fields) {
   }
 });
 connection.end();
+
+for (var i = 0; i < array.length; i++) {
+  array[i]
+}
