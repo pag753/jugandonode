60e480ae868defdb506f17ac730d80106c8c1320
Pablo de Jesús <pag753@gmail.com>
2018-11-28 16:05:29 -0600

    Server.

--- index.js
+++ index.js
@@ -1,37 +1,57 @@
-
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
+//Servidor HTTP
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+//MYSQL o MARIADB
+let mysql      = require('mysql');
+let connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
   database : 'classicmodels'
 });
-
 connection.connect();
-
-connection.query('SELECT * FROM customers', function (error, results, fields) {
+connection.query('SELECT * FROM employees', function (error, results, fields) {
   if (error) throw error;
-  //console.log(results);
-  //console.log(results[0]);
-  for (var i = 0; i < results.length; i++)
-  {
-    //console.log("Registro ",(i + 1),": ",results[i]);
-    console.log("Nombre: ",results[i].customerName);
-  }
+  //const server = http.createServer((req, res) => {
+    const server = http.createServer(function(req, res) {
+      res.statusCode = 200;
+
+    if (req.method == 'GET') {
+        res.setHeader('Content-Type', 'text/HTML; charset=utf-8');
+      let cadena = "";
+        for (let i = 0; i < results.length; i++) {
+            cadena += JSON.stringify(results[i]) + ",\n";
+        }
+        //res.end(cadena);
+        res.end("" +
+            "<form method='post'>" +
+            "  <input type='text' name='texto'>" +
+            "  <input type='submit' value='Aceptar' name='botón'>" +
+            "</form>" +
+            "")
+    }
+    if (req.method == 'POST') {
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString(); // convert Buffer to string
+        });
+        req.on('end', () => {
+            console.log(body);
+            res.end('ok');
+        });
+      //res.end("ES POR POST")
+        //console.log(req.on())
+    }
+    console.log(`${req.method}\n`)
+
+    //res.end(`<script>console.log(${cadena})</script>`);
+    //res.end('Hello, World!\n');
+  });
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+    //console.log(results);
+  });
 });
 connection.end();
-
-for (var i = 0; i < array.length; i++) {
-  array[i]
-}
-
-
-if (true) {
-  switch (expression) {
-    case expression:
-
-      break;
-    default:
-
-  }
-}
