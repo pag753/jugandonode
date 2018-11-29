121df39acb61d1b055c2ccaaaf5147e3f6df6a09
Pablo de Jesús <pag753@gmail.com>
2018-11-29 16:14:18 -0600

    Ejemplo primero de express.

--- index.js
+++ index.js
@@ -1,57 +1,21 @@
-//Servidor HTTP
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-//MYSQL o MARIADB
-let mysql      = require('mysql');
-let connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'classicmodels'
-});
-connection.connect();
-connection.query('SELECT * FROM employees', function (error, results, fields) {
-  if (error) throw error;
-  //const server = http.createServer((req, res) => {
-    const server = http.createServer(function(req, res) {
-      res.statusCode = 200;
+const express = require('express')
+const app = express()
+app.use(express.urlencoded())
+app.get('/', (req, res) => {
+    res.send('' +
+        '<form method="post">' +
+          '<input type="text" placeholder="Nombre" name="nombre">'+
+          '<input type="text" placeholder="Apellido paterno" name="ap">'+
+          '<input type="text" placeholder="Apellido materno" name="am">'+
+          '<input type="submit" value="Aceptar">'+
+        '</form>'
+    )
+    console.log(req.query)
+})
 
-    if (req.method == 'GET') {
-        res.setHeader('Content-Type', 'text/HTML; charset=utf-8');
-      let cadena = "";
-        for (let i = 0; i < results.length; i++) {
-            cadena += JSON.stringify(results[i]) + ",\n";
-        }
-        //res.end(cadena);
-        res.end("" +
-            "<form method='post'>" +
-            "  <input type='text' name='texto'>" +
-            "  <input type='submit' value='Aceptar' name='botón'>" +
-            "</form>" +
-            "")
-    }
-    if (req.method == 'POST') {
-        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString(); // convert Buffer to string
-        });
-        req.on('end', () => {
-            console.log(body);
-            res.end('ok');
-        });
-      //res.end("ES POR POST")
-        //console.log(req.on())
-    }
-    console.log(`${req.method}\n`)
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send("POR POST")
+})
 
-    //res.end(`<script>console.log(${cadena})</script>`);
-    //res.end('Hello, World!\n');
-  });
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-    //console.log(results);
-  });
-});
-connection.end();
+app.listen(3000, () => console.log('Server ready'))
